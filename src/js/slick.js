f9e3f9b1c762038552cda19210f8948b41416d98
Maksym Nazhyvotov <Zerzen1992@gmail.com>
2022-12-10 10:59:16 +0200

    сделал половинчатый слайдер

--- src/js/slick.js
+++ src/js/slick.js
@@ -3,5 +3,16 @@
 $('.about-restaurant-slaider').slick({
   infinite: true,
   slidesToShow: 2,
-  slidesToScroll: 2,
+  slidesToScroll: 1,
+  // autoplay: true,
+});
+
+$(document).ready(function () {
+  $('.restaurant-about-slider').slick({
+    arrows: true,
+    slidesToShow: 2,
+    variableWidth: true,
+    appendArrows: $('.restaurant-about__slider-nav'),
+    speed: 300,
+  });
 });
