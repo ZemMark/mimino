f883144a0aedeb3dfb8b7b7ab6528b873adf3328
ZemMark <111459434+ZemMark@users.noreply.github.com>
2022-12-12 13:15:04 +0000

    Deploying to gh-pages from @ ZemMark/mimino@ca88d516a80e2cdaa24dd5198c275bda80db952a 🚀

---
+++ index.0e0ad611.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".about-restaurant-slider").slick({arrows:!0,slidesToShow:2,variableWidth:!0,appendArrows:$(".lux-room__slider-nav"),speed:300,autoplay:!0})}));
+//# sourceMappingURL=index.0e0ad611.js.map
