df69186a96ca85009d2535925545e3a262939df7
Mark Zemlianskyi <111459434+ZemMark@users.noreply.github.com>
2022-12-14 13:37:58 +0200

    Revert "Main refactoring + contacts"

--- src/js/slick-lux-room.js
+++ src/js/slick-lux-room.js
@@ -7,12 +7,37 @@
 // });
 
 $(document).ready(function () {
-    $('.lux-room-slider').slick({
-      arrows: true,
-      slidesToShow: 2,
-      variableWidth: true,
-      appendArrows: $('.lux-room__slider-nav'),
-      speed: 300,
-      autoplay: true,
-    });
+  $('.lux-room-slider.lux-room').slick({
+    arrows: true,
+    slidesToShow: 2,
+    variableWidth: true,
+    appendArrows: $('.lux-room__slider-nav.lux-room'),
+    speed: 300,
+    prevArrow: '.lux-room-slider__arrow-left.lux-room',
+    nextArrow: '.lux-room-slider__arrow-right.lux-room',
   });
+});
+
+$(document).ready(function () {
+  $('.lux-room-slider.room-3').slick({
+    arrows: true,
+    slidesToShow: 2,
+    variableWidth: true,
+    appendArrows: $('.lux-room__slider-nav.room-3'),
+    speed: 300,
+    prevArrow: '.lux-room-slider__arrow-left.room-3',
+    nextArrow: '.lux-room-slider__arrow-right.room-3',
+  });
+});
+
+$(document).ready(function () {
+  $('.lux-room-slider.room-2').slick({
+    arrows: true,
+    slidesToShow: 2,
+    variableWidth: true,
+    appendArrows: $('.lux-room__slider-nav.room-2'),
+    speed: 300,
+    prevArrow: '.lux-room-slider__arrow-left.room-2',
+    nextArrow: '.lux-room-slider__arrow-right.room-2',
+  });
+});
