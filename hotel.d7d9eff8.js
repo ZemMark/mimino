2684554e1e103043d3a264deb38ea9a6379d35c6
ZemMark <111459434+ZemMark@users.noreply.github.com>
2022-12-14 10:52:55 +0000

    Deploying to gh-pages from @ ZemMark/mimino@962bce8f3d1a57058f78322ad0baf14d622d5e98 🚀

---
+++ hotel.d7d9eff8.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".lux-room-slider").slick({arrows:!0,slidesToShow:2,variableWidth:!0,appendArrows:$(".lux-room__slider-nav"),speed:300,autoplay:!0})}));
+//# sourceMappingURL=hotel.d7d9eff8.js.map
